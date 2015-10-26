849d39c248d5176236156ae316bd17c2af8ac3e8
gs-akhan <akhan@gainsight.com>
2015-10-26 10:14:41 +0530

    Tests now passing

--- __tests__/test.js
+++ __tests__/test.js
@@ -2,13 +2,15 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var TestUtils = require('react-addons-test-utils');
 
-jest.dontMock('../components/Home');
+jest.autoMockOff();
 
-describe('sum', function() {
- it('adds 1 + 2 to equal 3', function() {
-	var Home = require('../components/Home');
-   //var _Home = TestUtils.renderIntoDocument(<Home>);
-   //var homeNode = ReactDOM.findDOMNode(_Home);
+var Home = require('../components/Home');
+
+describe('Home', function() {
+ it('Rendering home component', function() {
+   
+   var _Home = TestUtils.renderIntoDocument(<Home />);
+   var homeNode = ReactDOM.findDOMNode(_Home);
    expect(2).toEqual(2);
  });
 });
